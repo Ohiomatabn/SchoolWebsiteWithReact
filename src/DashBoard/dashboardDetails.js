391233b0173844e7776109f185af6f5b7618b652
Ohiomata BN <benjaminohiomatanathaniel@gmail.com>
2026-03-14 14:36:48 +0100

    create an array of objects to store the dashboard content

---
+++ src/DashBoard/dashboardDetails.js
@@ -0,0 +1,42 @@
+export const dashBoardDetails = [
+  {
+    name: "My Profile",
+    image: "my_profile.png",
+    url: "../HTML/profile.html",
+  },
+  {
+    name: "Fee Allocation",
+    image: "naira.png",
+    url: "#",
+  },
+  {
+    name: "Fees Payment",
+    image: "school_fees.png",
+    url: "#",
+  },
+  {
+    name: "Outstanding Bills",
+    image: "debt.png",
+    url: "#",
+  },
+  {
+    name: "Result",
+    image: "result.png",
+    url: "../HTML/result.html",
+  },
+  {
+    name: "Staffs",
+    image: "staffs.png",
+    url: "#",
+  },
+  {
+    name: "Transfer",
+    image: "transfer.png",
+    url: "#",
+  },
+  {
+    name: "Reset Password",
+    image: "password.png",
+    url: "#",
+  },
+];
