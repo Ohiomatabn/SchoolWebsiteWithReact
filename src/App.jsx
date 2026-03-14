a0d2a4fa15df735b82e47674d27550b738fcfc5f
Ohiomata BN <benjaminohiomatanathaniel@gmail.com>
2026-03-14 14:37:09 +0100

    import all created component and render it

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-function App() {}
+import { NavBar } from "./component/NavBar";
+import { SideBar } from "./component/SideBar";
+import { DashBoard } from "./DashBoard/DashBoard";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+
+      <div className="divider">
+        <SideBar />
+        <DashBoard />
+      </div>
+    </>
+  );
+}
 
 export default App;
