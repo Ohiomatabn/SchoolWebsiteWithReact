d96b7b0701fab6357de09d4f73bab29dc1c4329c
Ohiomata BN <benjaminohiomatanathaniel@gmail.com>
2026-03-17 07:48:49 +0100

    change all the class to className

--- src/pages/profile/ProfileMain.jsx
+++ src/pages/profile/ProfileMain.jsx
@@ -5,8 +5,8 @@ export function ProfileMain() {
     <main>
       <DashBoardHeader />
 
-      <div class="student-data">
-        <ul class="bio-data">
+      <div className="student-data">
+        <ul className="bio-data">
           <h2>Bio-Data</h2>
           <li>Surname: David</li>
           <li>First Name: John</li>
@@ -16,14 +16,14 @@ export function ProfileMain() {
           <li>Nationality: Nigeria</li>
           <li>Other Name(s): Adavize</li>
         </ul>
-        <ul class="birthday">
+        <ul className="birthday">
           <h2>Birthday</h2>
           <li>Age: 20</li>
           <li>Date: 21</li>
           <li>Month: January</li>
           <li>Year: 2005</li>
         </ul>
-        <ul class="Contact">
+        <ul className="Contact">
           <h2>Contact</h2>
           <li>Student Phone No: 09161098199</li>
           <li>Home Address: Utoboo Obeiba Ihima</li>
@@ -31,19 +31,19 @@ export function ProfileMain() {
           <li>Mother's Phone No: 07047940700</li>
           <li>Parent Occupation: Bussiness and Farming</li>
         </ul>
-        <ul class="academics">
+        <ul className="academics">
           <h2>Academics</h2>
           <li>Registration No: 2021937ST110</li>
-          <li>Class: SSS 3</li>
+          <li>className: SSS 3</li>
           <li>Term: First Term</li>
         </ul>
-        <ul class="sponsor">
+        <ul className="sponsor">
           <h2>Sponsor</h2>
           <li>Name: Abraham John</li>
           <li>Phone No: 08130345282</li>
           <li>Relationship: Father</li>
         </ul>
-        <ul class="registered">
+        <ul className="registered">
           <h2>Registered</h2>
           <li>Date: 12</li>
           <li>Month: September</li>
