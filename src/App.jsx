3dc7766d09b19dbcddb8d84a757a12f4a0db9b11
Ohiomata BN <benjaminohiomatanathaniel@gmail.com>
2026-03-17 07:47:46 +0100

    add result route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import { HomePage } from "./pages/HomePage";
 import { Profile } from "./pages/profile/Profile";
+import { Result } from "./pages/Result/Result";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/result" element={<Result />} />
     </Routes>
   );
 }
